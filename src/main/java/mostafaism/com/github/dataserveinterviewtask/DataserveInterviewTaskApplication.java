5f2e7775fe68194bb0534dc536e174ce7fcf154b
mostafa <me.cssign@gmail.com>
2021-06-13 08:17:57 +0200

    Initialize spring boot project

---
+++ src/main/java/mostafaism/com/github/dataserveinterviewtask/DataserveInterviewTaskApplication.java
@@ -0,0 +1,13 @@
+package mostafaism.com.github.dataserveinterviewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataserveInterviewTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataserveInterviewTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mostafaism/com/github/dataserveinterviewtask/DataserveInterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package mostafaism.com.github.dataserveinterviewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataserveInterviewTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

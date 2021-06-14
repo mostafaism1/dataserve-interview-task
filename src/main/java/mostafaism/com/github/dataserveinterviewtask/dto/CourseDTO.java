6fc4e220937983a3ec01d5d3892cf1881dc563dc
mostafa <me.cssign@gmail.com>
2021-06-14 18:52:38 +0200

    Create a Course DTO

---
+++ src/main/java/mostafaism/com/github/dataserveinterviewtask/dto/CourseDTO.java
@@ -0,0 +1,15 @@
+package mostafaism.com.github.dataserveinterviewtask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseDTO {
+    private Long id;
+    private String name;
+}

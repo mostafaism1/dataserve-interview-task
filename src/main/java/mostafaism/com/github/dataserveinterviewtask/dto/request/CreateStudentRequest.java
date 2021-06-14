fd86090720070d254f96f9ff665afe9ecbcc4cf3
mostafa <me.cssign@gmail.com>
2021-06-14 18:53:15 +0200

    Create a CreateStudentRequest class

---
+++ src/main/java/mostafaism/com/github/dataserveinterviewtask/dto/request/CreateStudentRequest.java
@@ -0,0 +1,18 @@
+package mostafaism.com.github.dataserveinterviewtask.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateStudentRequest {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phone;
+}

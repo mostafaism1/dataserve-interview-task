964ae2777011177a28fc0a7f34aa0b16ca87053f
mostafa <me.cssign@gmail.com>
2021-06-14 19:23:54 +0200

    Increment sequence after inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,72 +6,90 @@ values (
         'California',
         'john1@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         2,
         'John',
         'Doe',
         'California',
         'john2@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         3,
         'John',
         'Doe',
         'California',
         'john3@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         4,
         'John',
         'Doe',
         'California',
         'john4@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         5,
         'John',
         'Doe',
         'California',
         'john5@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         6,
         'John',
         'Doe',
         'California',
         'john6@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         7,
         'John',
         'Doe',
         'California',
         'john7@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         8,
         'John',
         'Doe',
         'California',
         'john8@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         9,
         'John',
         'Doe',
         'California',
         'john9@email.com',
         '123456789'
-    ),
-    (
+    );
+call next value for hibernate_sequence;
+insert into student(id, first_name, last_name, address, email, phone)
+VALUES (
         10,
         'John',
         'Doe',
@@ -79,14 +97,34 @@ values (
         'john10@email.com',
         '123456789'
     );
+call next value for hibernate_sequence;
 insert into course(id, name)
-values (1, 'course1'),
-    (2, 'course2'),
-    (3, 'course3'),
-    (4, 'course4'),
-    (5, 'course5'),
-    (6, 'course6'),
-    (7, 'course7'),
-    (8, 'course8'),
-    (9, 'course9'),
-    (10, 'course10');
+values (1, 'course1');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values(2, 'course2');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values(3, 'course3');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (4, 'course4');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (5, 'course5');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (6, 'course6');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (7, 'course7');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (8, 'course8');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (9, 'course9');
+call next value for hibernate_sequence;
+insert into course(id, name)
+values (10, 'course10');
+call next value for hibernate_sequence;

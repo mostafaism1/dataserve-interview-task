039571fbc05146e071571f11452fd62cead50f6a
mostafa <me.cssign@gmail.com>
2021-06-14 19:04:20 +0200

    Seed the db with test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,92 @@
+insert into student(id, first_name, last_name, address, email, phone)
+values (
+        1,
+        'John',
+        'Doe',
+        'California',
+        'john1@email.com',
+        '123456789'
+    ),
+    (
+        2,
+        'John',
+        'Doe',
+        'California',
+        'john2@email.com',
+        '123456789'
+    ),
+    (
+        3,
+        'John',
+        'Doe',
+        'California',
+        'john3@email.com',
+        '123456789'
+    ),
+    (
+        4,
+        'John',
+        'Doe',
+        'California',
+        'john4@email.com',
+        '123456789'
+    ),
+    (
+        5,
+        'John',
+        'Doe',
+        'California',
+        'john5@email.com',
+        '123456789'
+    ),
+    (
+        6,
+        'John',
+        'Doe',
+        'California',
+        'john6@email.com',
+        '123456789'
+    ),
+    (
+        7,
+        'John',
+        'Doe',
+        'California',
+        'john7@email.com',
+        '123456789'
+    ),
+    (
+        8,
+        'John',
+        'Doe',
+        'California',
+        'john8@email.com',
+        '123456789'
+    ),
+    (
+        9,
+        'John',
+        'Doe',
+        'California',
+        'john9@email.com',
+        '123456789'
+    ),
+    (
+        10,
+        'John',
+        'Doe',
+        'California',
+        'john10@email.com',
+        '123456789'
+    );
+insert into course(id, name)
+values (1, 'course1'),
+    (2, 'course2'),
+    (3, 'course3'),
+    (4, 'course4'),
+    (5, 'course5'),
+    (6, 'course6'),
+    (7, 'course7'),
+    (8, 'course8'),
+    (9, 'course9'),
+    (10, 'course10');
